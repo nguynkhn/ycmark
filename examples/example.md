f7bb5638a74f751bcd0a73bbf52cdeb10f19cb5b
nguynkhn <nguynkhan479@gmail.com>
2025-07-09 21:21:03 +0700

    add cli and proper yaml parsing

---
+++ examples/example.md
@@ -0,0 +1,14 @@
+---
+title: Hello ycmark
+date: 20250709
+
+tools:
+    terminal: Termux
+    editor: Vim
+...
+# Lorem ipsum
+
+1. Item 1
+2. Item 2
+    - Item 2.1
+    - Item 2.2
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,55 @@
+use clap::Parser as CliParser;
 use pulldown_cmark::{CowStr, Event, Options, Parser, Tag};
 use yaml_rust2::{Yaml, YamlLoader};
 
 use std::collections::HashMap;
+
+fn stringify(root: &Yaml) -> HashMap<String, String> {
+    let mut map = HashMap::new();
+    let mut queue = vec![];
+
+    queue.push(("".to_string(), root));
+
+    while let Some((key, node)) = queue.pop() {
+        let value = match node {
+            Yaml::Hash(h) => {
+                for (name, node) in h.into_iter() {
+                    let name = name.clone().into_string().unwrap();
+                    let key = if key.is_empty() {
+                        name
+                    } else {
+                        format!("{key}.{name}")
+                    };
+
+                    queue.push((key, node))
+                }
+                continue;
+            }
+            Yaml::Array(a) => {
+                for (index, node) in a.iter().enumerate() {
+                    let key = if key.is_empty() {
+                        index.to_string()
+                    } else {
+                        format!("{key}.{index}")
+                    };
+
+                    queue.push((key, node))
+                }
+                continue;
+            }
+            Yaml::Real(r) => r.to_string(),
+            Yaml::Integer(i) => i.to_string(),
+            Yaml::String(s) => s.clone(),
+            Yaml::Boolean(b) => b.to_string(),
+            Yaml::Null => "null".to_string(),
+            _ => "".to_string(),
+        };
+
+        map.insert(key, value);
+    }
+
+    map
+}
 
 fn convert(input: &str, template: Option<String>) -> String {
     let options = Options::ENABLE_YAML_STYLE_METADATA_BLOCKS;
@@ -16,28 +64,13 @@ fn convert(input: &str, template: Option<String>) -> String {
     }
 
     pulldown_cmark::html::push_html(&mut html_output, parser);
+    html_output = html_output.trim().to_owned();
 
     if let Some(yaml_block) = yaml_block {
         let yaml = YamlLoader::load_from_str(&yaml_block).unwrap();
-        let root = yaml[0].as_hash().unwrap();
+        let root = &yaml[0];
 
-        let mut metadata: HashMap<String, String> = root
-            .into_iter()
-            .map(|(key, value)| {
-                let key = key.clone().into_string().unwrap();
-                let value = match value {
-                    Yaml::Real(r) => r.to_string(),
-                    Yaml::Integer(i) => i.to_string(),
-                    Yaml::String(s) => s.clone(),
-                    Yaml::Boolean(b) => b.to_string(),
-                    Yaml::Null => "null".to_string(),
-                    _ => "".to_string(),
-                };
-
-                (key, value)
-            })
-            .collect();
-
+        let mut metadata = stringify(root);
         metadata.insert("body".to_string(), html_output.clone());
 
         if let Some(template) = template {
@@ -52,5 +85,36 @@ fn convert(input: &str, template: Option<String>) -> String {
     html_output
 }
 
+#[derive(Debug, CliParser)]
+#[command(version, about)]
+struct Args {
+    #[arg(value_name = "FILE")]
+    file: Option<String>,
+
+    #[arg(short = 'T', long, value_name = "FILE")]
+    template: Option<String>,
+
+    #[arg(short, long, value_name = "FILE")]
+    output: Option<String>,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    let input = if let Some(file) = args.file {
+        std::fs::read_to_string(file).expect("failed to read from file")
+    } else {
+        let stdin = std::io::stdin();
+        std::io::read_to_string(stdin).expect("failed to read from stdin")
+    };
+    let template = args
+        .template
+        .map(|path| std::fs::read_to_string(path).expect("failed to read template file"));
+
+    let result = convert(&input, template);
+    if let Some(file) = args.output {
+        std::fs::write(file, result).expect("failed to write to file");
+    } else {
+        println!("{result}");
+    }
 }
